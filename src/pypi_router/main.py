ed42d821101eb7858d80ec9357a70874645cf591
zhaochow <zhaowen04@gmail.com>
2024-05-26 08:54:22 +0900

    (WIP) feat: Redesign app inputs/args, use simpleindex only
    - App run with either list of packages, local PyPI index or simpleindex config
    - Use simpleindex routing for serving local index

--- src/pypi_router/main.py
+++ src/pypi_router/main.py
@@ -1,7 +1,5 @@
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from pathlib import Path
-import subprocess
-import sys
 
 import simpleindex
 from simpleindex.routes import Response, Route, Params
@@ -44,65 +42,36 @@ def main(*args):
     global _cache_dir
     if len(args) < 1: args = None
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    group = parser.add_mutually_exclusive_group(required=True)
-    # group.add_argument('--pypi-repo') # TODO
-    group.add_argument('--pypi-path', help='Path to custom index')
-    parser.add_argument('--source', choices=['http', 'path'], default='http',
-                        help='simpleindex source')
-    parser.add_argument('--pypi-port', type=int, default=8001,
-                        help='http.server port')
+    parser.add_argument('-l', '--package-list')
+    parser.add_argument('-i', '--pypi-index', help='Path to custom index')
+    parser.add_argument('-c', '--config', help='simpleindex configuration')
     parser.add_argument('--cache-dir', default=str(_cache_dir),
                         help='Cache directory')
-    parser.add_argument('--config', help='simpleindex configuration')
     parser.add_argument('--port', type=int, help='simpleindex port')
     args = parser.parse_args(args)
 
     _cache_dir = Path(args.cache_dir).resolve()
 
-    if args.pypi_path is None:
-        raise NotImplementedError()
-        p = subprocess.run(['git', 'clone', args.pypi_repo, str(cache_dir)],
-                           check=True)
-        pypi_root
-    else:
-        pypi_root = Path(args.pypi_path).resolve()
-
     if args.config is None:
-        packages = [p for p in pypi_root.glob('*') if p.is_dir()]
-
-        with open(Path(__file__).with_name('config_template.toml')) as f:
-            cfg = f.readlines()
-
-        source_str = f"source = \"{args.source}\"\n"
-        custom_lines = []
-        for package in packages:
-            custom_lines.extend([
-                f"[routes.\"{package.name}\"]\n",
-                source_str,
-                f"to = \"http://127.0.0.1:{args.pypi_port}/{package.name}/\"\n",
-                '\n',
-            ])
-        cfg = cfg[:1] + custom_lines + cfg[2:]
-
-        if args.port is not None:
-            cfg[-1] = f"port = {args.port}\n"
-
-        _cache_dir.mkdir(parents=True, exist_ok=True)
-        cfg_path = _cache_dir.joinpath('config.toml')
-        with open(cfg_path, 'w', encoding='utf-8') as f:
-            f.writelines(cfg)
+        if args.pypi_index is None:
+            if args.package_list is None:
+                raise ValueError('At least one of (--package-list | '
+                                 '--pypi-index | --config) should be given')
+            pypi_index = ut.create_index(args.package_list,
+                                         cache_dir=_cache_dir)
+        else:
+            pypi_index = Path(args.pypi_index).resolve()
+            if not pypi_index.is_dir():
+                if args.package_list is None:
+                    raise FileExistsError(f"{str(pypi_index)} does not exist")
+                ut.create_index(args.package_list, index_dir=pypi_index,
+                                cache_dir=_cache_dir)
+        cfg_path = ut.create_config(pypi_index, port=args.port,
+                                    cache_dir=_cache_dir)
     else:
         cfg_path = Path(args.config).resolve()
 
-    pypi_server = subprocess.Popen(
-        [sys.executable, '-m', 'http.server', '--directory', str(pypi_root),
-         str(args.pypi_port)]
-    )
-
-    simpleindex_server = subprocess.Popen(
-        [sys.executable, '-m', 'simpleindex', str(cfg_path)]
-    )
-    pass
+    simpleindex.run([str(cfg_path)])
 
 if __name__ == '__main__':
     main()
