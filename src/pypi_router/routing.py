4f4d8b8f0982eb5907fb3c58805b8c3a0f54e0d5
zhaochow <zhaowen04@gmail.com>
2024-06-09 21:19:15 +0900

    fix: Run simpleindex with correct user cache dir

--- src/pypi_router/routing.py
+++ src/pypi_router/routing.py
@@ -42,3 +42,35 @@ class LocalIndexRoute(Route):
         data = path.read_bytes()
         return Response(status_code=200, content=data, media_type=media_type)
 
+# simpleindex.__main__.py
+def _build_routes(key: str, route: Route, cache_dir: Path):
+    async def page(request: Request):
+        response = await route.get_page(request.path_params)
+        return response.to_http_response()
+
+    async def dist(request: Request):
+        params = request.path_params
+        filename = params.pop(filename_param)
+        if isinstance(route, LocalIndexRoute):
+            response = await route.get_file(params, filename, cache_dir)
+        else:
+            response = await route.get_file(params, filename)
+        return response.to_http_response()
+
+    filename_param = "__simpleindex_match_filename__"
+    return [
+        routing.Route(f"/{key}/", page),
+        routing.Route(f"/{key}/{{{filename_param}}}", dist),
+    ]
+
+def run_simpleindex(config: str, cache_dir: Path):
+    config_path, configuration = Configuration.parse_arg(config)
+
+    routes = itertools.chain.from_iterable(
+        _build_routes(key, route.derive(config_path.parent), cache_dir)
+        for key, route in configuration.routes.items()
+    )
+    app = Starlette(routes=list(routes))
+
+    options = {k.replace("-", "_"): v for k, v in configuration.server.items()}
+    run_uvicorn(app, **options)
